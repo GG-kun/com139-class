e1d679441b07b4d501b70bbd632ef5615545a7e8
Guillermo González <0205838@up.edu.mx>
2021-02-06 11:40:20 -0600

    Feature: Rules and Size

--- GoL/conway.py
+++ GoL/conway.py
@@ -23,11 +23,58 @@ def addGlider(i, j, grid):
                        [0,  255, 255]])
     grid[i:i+3, j:j+3] = glider
 
+def addBlock(i, j, grid):
+    """adds a block with top left cell at (i, j)"""
+    block = np.array([[255, 255], 
+                        [255, 255]])
+    grid[i:i+2, j:j+2] = block
+
+def addBlinker(i, j, grid):
+    """adds a blinker with top left cell at (i, j)"""
+    blinker = np.array([[0,    255, 0], 
+                        [0,  255, 0], 
+                        [0,  255, 0]])
+    grid[i:i+3, j:j+3] = blinker
+
+def live_rule(cell, neighborsCount):
+    if neighborsCount < 2 or neighborsCount > 3:
+        return OFF
+    return cell
+
+def dead_rule(cell, neighborsCount):
+    if neighborsCount == 3:
+        return ON
+    return cell
+
 def update(frameNum, img, grid, N):
     # copy grid since we require 8 neighbors for calculation
     # and we go line by line 
     newGrid = grid.copy()
     # TODO: Implement the rules of Conway's Game of Life
+    yCells = len(grid)
+    for y in range(yCells):
+        xCells = len(grid[y])
+        for x in range(xCells):
+            cell = grid[y][x]
+            # Get neighbors list
+            neighbors = []
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    if i == 0 and j == 0:
+                        continue
+                    neighborY = y+i
+                    neighborX = x+j
+                    if neighborY >= 0 and neighborY < yCells and neighborX >= 0 and neighborX < xCells:
+                        neighbors.append(grid[neighborY][neighborX])
+            # Get neighbors count
+            neighborsCount = neighbors.count(ON)
+            # Live rule
+            if cell == ON:
+                cell = live_rule(cell, neighborsCount)
+            # Dead rule    
+            else:
+                cell = dead_rule(cell, neighborsCount)
+            newGrid[y][x] = cell
 
     # update data
     img.set_data(newGrid)
@@ -40,10 +87,18 @@ def main():
     # sys.argv[0] is the script name itself and can be ignored
     # parse arguments
     parser = argparse.ArgumentParser(description="Runs Conway's Game of Life system.py.")
-    # TODO: add arguments
     
     # set grid size
     N = 100
+    if len(sys.argv) > 1:
+        N = sys.argv[1]
+        if N.isnumeric():
+            N = int(N)
+        else:
+            print("usage: conway.py [size] [generation] [file]")
+            return
+        if len(sys.argv) > 2:
+            configurationFile = sys.argv[2]    
         
     # set animation update interval
     updateInterval = 50
@@ -51,10 +106,16 @@ def main():
     # declare grid
     grid = np.array([])
     # populate grid with random on/off - more off than on
-    grid = randomGrid(N)
+    # grid = randomGrid(N)
     # Uncomment lines to see the "glider" demo
-    #grid = np.zeros(N*N).reshape(N, N)
-    #addGlider(1, 1, grid)
+    # grid = np.zeros(N*N).reshape(N, N)
+    # addGlider(1, 1, grid)
+    # Uncomment lines to see the "block" demo
+    # grid = np.zeros(N*N).reshape(N, N)
+    # addBlock(1, 1, grid)
+    # Uncomment lines to see the "blinker" demo
+    # grid = np.zeros(N*N).reshape(N, N)
+    # addBlinker(1, 1, grid)
 
     # set up animation
     fig, ax = plt.subplots()
